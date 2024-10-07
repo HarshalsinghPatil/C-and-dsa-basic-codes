714cc41a0d067eeae979dbc1cbfb664fc6f45e01
harshalsingh.patil <harshalsingh.patil@STSPC80>
2024-10-07 20:04:45 +0530

    Simple Queue implemented

---
+++ DSA by me in c#/2. Queue/1.SimpleQueue/1.SimpleQueue/Program.cs
@@ -0,0 +1,99 @@
+﻿using _1.SimpleQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleQueue q = null;
+        int size;
+        int choice;
+        int element;
+
+        Console.WriteLine("Enter the size of Array");
+        size = int.Parse(Console.ReadLine());
+        q = new SimpleQueue(size);
+
+        do
+        {
+            Console.WriteLine("\n1. Enqueue\n2. Dequeue\n3. Element at Front\n4. Element at Rear\n5. Print Queue Data\n0. Exit\n: ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+
+                case 1:
+                    if(!q.IsFull())
+                    {
+                        Console.WriteLine("To enter the element");
+                        element = int.Parse(Console.ReadLine());
+                        q.Enqueue(element);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue is full ");
+                    }
+                    break;
+
+                case 2:
+                    if (!q.IsEmpty())
+                    {
+
+                        Console.WriteLine("Element popped : " + q.Dequeue());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue is Empty ");
+                    }
+
+                    break;
+
+                case 3:
+                    if (!q.IsEmpty())
+                    {
+
+                        Console.WriteLine("Element at Front : " + q.ElementAtFront());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue is Empty ");
+                    }
+
+                    break;
+
+                case 4:
+
+                    if (!q.IsEmpty())
+                    {
+
+                        Console.WriteLine("Element at Rear : " + q.ElementAtRear());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue is Empty ");
+                    }
+                    break;
+
+                case 5:
+                    q.PrintAll();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Wait .... Program exiting");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Please enter the right choice ");
+                    break;
+
+            }
+
+        } while (choice != 0);
+
+    }
+
+}
---
+++ DSA by me in c#/2. Queue/1.SimpleQueue/1.SimpleQueue/SimpleQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.SimpleQueue
+{
+    public  class SimpleQueue
+    {
+        int[] queue;
+        int front;
+        int rear;
+        int maxSize;
+        
+
+        public SimpleQueue (int size)
+        {
+            maxSize = size;
+            front = 0;
+            rear = -1;
+            queue = new int[maxSize];
+        }
+
+        public void Enqueue (int element )
+        {
+            ++rear;
+            queue[rear] = element;
+        }
+
+        public int Dequeue ()
+        {
+            return queue[front++];
+        }
+
+        public int ElementAtFront ()
+        {
+            return queue[front];
+        }
+
+        public int ElementAtRear()
+        {
+            return queue[rear];
+        }
+
+        public void PrintAll()
+        {
+            int index = 0;
+            for (int i = 0; i < queue.Length; i++)
+            {
+                Console.WriteLine($"The value of Queue at index queue[{index}] is {queue[i]}");
+                index++;
+            }
+        }
+        public bool IsFull()
+        {
+            return rear == maxSize - 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return front > rear;
+        }
+
+
+
+
+    }
+}
