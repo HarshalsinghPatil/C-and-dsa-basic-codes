be6d1c19c41770482b9899092fadcf91395dfed2
harshalsingh.patil <harshalsingh.patil@STSPC80>
2024-10-07 11:38:39 +0530

    Stack in c# and changed folder name

---
+++ DSA by me in c#/1. Stack/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleStack stack = null;
+        int choice, size, element;
+
+        Console.WriteLine("Enter the size of stack ");
+        size = int.Parse(Console.ReadLine());
+        stack = new SimpleStack(size);
+
+        Console.WriteLine("The size of the stack is : " + size);
+        Console.WriteLine("Stack is empty ?" + stack.IsEmpty());
+        Console.WriteLine("Stack is full ?" + stack.IsFull());
+
+        do
+        {
+            Console.WriteLine("Please select from below options \n1. Push\n2. Pop\n3. Element at top\n4. Print Stack Data\n0. Exit\n: ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: //push
+                    if (!stack.IsFull())
+                    {
+                        Console.WriteLine("Enter the Element: ");
+                        element = int.Parse(Console.ReadLine());
+                        stack.Push(element);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't push , Stack is full ! ");
+                    }
+                    break;
+
+                case 2: //pop
+                    if (!stack.IsEmpty())
+                    {
+                        Console.WriteLine("The popped element is : " + stack.Pop());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry,  Stack is Empty ! ");
+                    }
+                    break;
+
+                case 3: //peek
+                    if (!stack.IsEmpty())
+                    {
+                        Console.WriteLine("The element at top is : " + stack.ElementAtTop());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry,  Stack is Empty ! ");
+                    }
+                    break;
+
+                case 4: //print all elements
+                    if (!stack.IsEmpty())
+                    {
+                        stack.PrintAll();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, Stack is Empty !");
+                    }
+                    break;
+                case 0: //exit the program 
+                    Console.WriteLine("Exiting the program ...!");
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong Input! Please select valid option");
+                    break;
+
+            }
+
+        } while (choice != 0);
+    }
+}
---
+++ DSA by me in c#/1. Stack/ConsoleApp1/ConsoleApp1/SimpleStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        public class SimpleStack
+        {// if you want to use public modefier for methods then you need to define class public 
+
+            int[] stack;
+            int maxSize;
+            int top;
+            //int maxSize , top;
+
+            public SimpleStack(int size)
+            {
+                maxSize = size;
+                top = -1;
+                stack = new int[maxSize];
+            }
+
+            //here in stack methods we have removed all the exceptions becuase it should be handled in program itself
+            public void Push(int element)
+            {
+                ++top;
+                stack[top] = element;
+            }
+
+            public int Pop()
+            {
+                return stack[top--];
+            }
+
+            public int ElementAtTop()//Peek
+            {
+                return stack[top];
+            }
+
+            public bool IsFull()
+            {
+                return top == maxSize - 1;
+            }
+
+            public bool IsEmpty()
+            {
+                return top == -1;
+            }
+
+            public void PrintAll()
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine($"The element at [{i}] is : {stack[i]}");
+                }
+            }
+
+        }
+    }
+
+/*   Theory */
+
+
+
